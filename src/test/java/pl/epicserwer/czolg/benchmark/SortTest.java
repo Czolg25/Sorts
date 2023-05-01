e544ea81e93aac14b9900e9f2a5cde0b1d154e4b
Czolg25 <69245647+Czolg25@users.noreply.github.com>
2023-05-01 11:02:11 +0200

    Add files via upload

--- src/test/java/pl/epicserwer/czolg/benchmark/SortTest.java
+++ src/test/java/pl/epicserwer/czolg/benchmark/SortTest.java
@@ -6,17 +6,21 @@ import pl.epicserwer.czolg.sort.utils.GenerateArray;
 import pl.epicserwer.czolg.sort.utils.sort.BubbleSort;
 import pl.epicserwer.czolg.sort.utils.sort.QuickSort;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class SortTest {
     private static final int ARRAY_LENGTH = 10_000;
+    private static final int NUMBER_OF_WARM = 5;
+    private static final int NUMBER_OF_FORK = 1;
+    private static final int NUMBER_OF_ITERATION = 5;
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @Warmup(iterations = 5)
-    @Fork(1)
-    @Measurement(iterations = 5)
+    @Warmup(iterations = NUMBER_OF_WARM)
+    @Fork(NUMBER_OF_FORK)
+    @Measurement(iterations = NUMBER_OF_ITERATION)
     public void sortBubbleSortRandomCase() {
         int[] arr = GenerateArray.generateRandomArray(ARRAY_LENGTH);
         BubbleSort.sort(arr);
@@ -25,9 +29,9 @@ public class SortTest {
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @Warmup(iterations = 5)
-    @Fork(1)
-    @Measurement(iterations = 5)
+    @Warmup(iterations = NUMBER_OF_WARM)
+    @Fork(NUMBER_OF_FORK)
+    @Measurement(iterations = NUMBER_OF_ITERATION)
     public void sortQuickSortRandomPivotRandomCase() {
         int[] arr = GenerateArray.generateRandomArray(ARRAY_LENGTH);
         QuickSort.sort(arr, QuickSortPivotType.RANDOM);
@@ -35,9 +39,9 @@ public class SortTest {
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @Warmup(iterations = 5)
-    @Fork(1)
-    @Measurement(iterations = 5)
+    @Warmup(iterations = NUMBER_OF_WARM)
+    @Fork(NUMBER_OF_FORK)
+    @Measurement(iterations = NUMBER_OF_ITERATION)
     public void sortQuickSortFirstPivotRandomCase() {
         int[] arr = GenerateArray.generateRandomArray(ARRAY_LENGTH);
         QuickSort.sort(arr, QuickSortPivotType.RANDOM);
@@ -45,11 +49,21 @@ public class SortTest {
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @Warmup(iterations = 5)
-    @Fork(1)
-    @Measurement(iterations = 5)
+    @Warmup(iterations = NUMBER_OF_WARM)
+    @Fork(NUMBER_OF_FORK)
+    @Measurement(iterations = NUMBER_OF_ITERATION)
     public void sortQuickSortLastPivotRandomCase() {
         int[] arr = GenerateArray.generateRandomArray(ARRAY_LENGTH);
         QuickSort.sort(arr, QuickSortPivotType.RANDOM);
+    }
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Warmup(iterations = NUMBER_OF_WARM)
+    @Fork(NUMBER_OF_FORK)
+    @Measurement(iterations = NUMBER_OF_ITERATION)
+    public void sortJavaSortRandomCase() {
+        int[] arr = GenerateArray.generateRandomArray(ARRAY_LENGTH);
+        Arrays.sort(arr);
     }
 }
